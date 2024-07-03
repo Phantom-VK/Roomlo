119377492e356320c64c45d2861d67c3a29b6f2e
Phantom-VK <vikramadityakhupse@gmail.com>
2024-07-03 11:05:38 +0530

    Added extra touch to Profile Screen

--- app/src/main/java/com/example/roomlo/ProfileScreen.kt
+++ app/src/main/java/com/example/roomlo/ProfileScreen.kt
@@ -1,22 +1,34 @@
 package com.example.roomlo
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.roomlo.ui.theme.baloo2Font
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun ProfileScreen(
     viewModel: RoomViewModel,
@@ -26,76 +38,115 @@ fun ProfileScreen(
     var address by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var phoneNumber by remember { mutableStateOf("") }
+    var wpNumber by remember { mutableStateOf("") }
+    var userId by remember { mutableStateOf("") }
 
-    Scaffold(
-        topBar = { AppTopBar(title = "Profile") },
-        containerColor = Color.White
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Color.White
+            ),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(20.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        Text(
+            text = "Profile",
+            fontFamily = baloo2Font,
+            fontWeight = FontWeight.Bold,
+            fontSize = 40.sp,
+            color = Color.Black
+        )
+        IconButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.requiredSize(150.dp)
         ) {
-            IconButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.requiredSize(150.dp)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.profile_pic),
-                    contentDescription = "Profile Picture",
-                    modifier = Modifier.requiredSize(180.dp),
-                    tint = Color(0xFFADC9FF)
-                )
-            }
-            Spacer(modifier = Modifier.height(30.dp))
+            Icon(
+                painter = painterResource(id = R.drawable.profile_pic),
+                contentDescription = "Profile Picture",
+                modifier = Modifier.requiredSize(110.dp),
+                tint = Color(0xFFADC9FF)
+            )
+        }
+        Spacer(modifier = Modifier.height(5.dp))
 
-            Text(
-                text = "Vikramaditya Khupse",
-                fontSize = 20.sp,
-                color = Color.DarkGray
-            )
+        Text(
+            text = "Vikramaditya Khupse",
+            fontSize = 20.sp,
+            color = Color.DarkGray
+        )
 
-            UnderlineTextField(
-                value = name,
-                onValueChange = { name = it },
-                hint = "Name"
-            )
-            UnderlineTextField(
-                value = address,
-                onValueChange = { address = it },
-                hint = "Address"
-            )
-            UnderlineTextField(
-                value = email,
-                onValueChange = { email = it },
-                hint = "Email ID"
-            )
-            UnderlineTextField(
-                value = phoneNumber,
-                onValueChange = { phoneNumber = it },
-                hint = "Phone Number"
-            )
+        UnderlineTextField(
+            value = name,
+            onValueChange = { name = it },
+            hint = "Name",
+            imageVector = Icons.Filled.AccountCircle
+        )
+        UnderlineTextField(
+            value = address,
+            onValueChange = { address = it },
+            hint = "Address",
+            imageVector = Icons.Filled.LocationOn
+        )
+        UnderlineTextField(
+            value = email,
+            onValueChange = { email = it },
+            hint = "Email ID",
+            imageVector = Icons.Outlined.Email
+        )
 
-            Button(
-                onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors()
-            ) {
-                Text(text = "Save")
-            }
+
+        UnderlineTextField(
+            value = phoneNumber,
+            onValueChange = { phoneNumber = it },
+            hint = "Phone Number",
+            imageVector = Icons.Filled.Call
+        )
+
+
+
+        UnderlineTextField(
+            value = wpNumber,
+            onValueChange = { wpNumber = it },
+            hint = "WhatsApp Number",
+            imageVector = Icons.Filled.Call
+        )
+        UnderlineTextField(
+            value = userId,
+            onValueChange = { userId = it },
+            hint = "UserId",
+            imageVector = Icons.Filled.Face
+        )
+
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors()
+        ) {
+            Text(text = "Save")
         }
     }
+
 }
+
 
 @Composable
 fun UnderlineTextField(
     value: String,
     onValueChange: (String) -> Unit,
-    hint: String
+    hint: String,
+    imageVector: ImageVector
 ) {
     var isHintDisplayed by remember { mutableStateOf(value.isEmpty()) }
-
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = "Image",
+            Modifier.requiredSize(40.dp), tint = Color.Black
+    )
     BasicTextField(
         value = value,
         onValueChange = {
@@ -103,24 +154,26 @@ fun UnderlineTextField(
             isHintDisplayed = it.isEmpty()
         },
         singleLine = true,
-        textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
+        textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp)
-    ) { innerTextField ->
+            .padding(top = 8.dp, start = 15.dp, end = 15.dp),
+
+        ) { innerTextField ->
         Column {
             if (isHintDisplayed) {
                 Text(
                     text = hint,
-                    style = TextStyle(color = Color.Gray, fontSize = 18.sp)
+                    style = TextStyle(color = Color.Gray, fontSize = 15.sp)
                 )
             }
             innerTextField()
             Divider(color = Color.Gray, thickness = 2.dp)
         }
     }
+
 }
 
+}
 
 
 @Preview(showBackground = true)
