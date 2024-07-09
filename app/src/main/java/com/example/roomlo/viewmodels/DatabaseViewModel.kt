ae98814b9d50d0e8a3d0fa5df70bfad9b691a6a2
Phantom-VK <vikramadityakhupse@gmail.com>
2024-07-09 13:29:58 +0530

    Integrated simple firebase user data upload mechanism

--- app/src/main/java/com/example/roomlo/viewmodels/DatabaseViewModel.kt
+++ app/src/main/java/com/example/roomlo/viewmodels/DatabaseViewModel.kt
@@ -9,91 +9,30 @@ import com.google.firebase.ktx.Firebase
 
 class DatabaseViewModel : ViewModel() {
 
-    private val database = Firebase.database
+    private val db = Firebase.firestore
+    private val tag = "DatabaseViewModel"
 
-    var myRef = database.getReference("User").setValue("New user")
-
-
-    // Create a new user with a first and last name
     private val user = hashMapOf(
-        "first" to "Ada",
-        "last" to "Lovelace",
-        "born" to 1815
+        "name" to "Vikram",
+        "mobilenumber" to "8530292951",
+        "isOwner" to false
     )
 
-// Add a new document with a generated ID
-val db = Firebase.firestore.collection("users")
-    .add(user)
-    .addOnSuccessListener { documentReference ->
-        Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-    }
-    .addOnFailureListener { e ->
-        Log.w(TAG, "Error adding document", e)
+
+
+    init {
+        addUserToDatabase()
     }
 
-//    fun addUser(user: User, context: Context) {
-//        uid?.let { userId ->
-//            val userMap = mapOf(
-//                "name" to user.name,
-//                "address" to user.address,
-//                "email" to user.email,
-//                "mobilenumber" to user.mobilenumber,
-//                "password" to user.password,
-//                "wpnumber" to user.wpnumber,
-//                "isOwner" to user.isOwner
-//            )
-//
-//            userRef.child(userId).setValue(userMap).addOnCompleteListener { task ->
-//                if (task.isSuccessful) {
-//                    Toast.makeText(context, "Profile updated successfully!", Toast.LENGTH_LONG).show()
-//                } else {
-//                    Toast.makeText(context, "Failed to update profile!", Toast.LENGTH_LONG).show()
-//                }
-//            }
-//        } ?: run {
-//            Toast.makeText(context, "User ID is null", Toast.LENGTH_LONG).show()
-//        }
-//    }
-//
-//    fun updateProfilePicture(uri: Uri, context: Context, onSuccess: (Uri) -> Unit, onFailure: (Exception) -> Unit) {
-//        uid?.let { userId ->
-//            val storageRef = FirebaseStorage.getInstance().getReference("Profile_pictures")
-//                .child(userId)
-//                .child("profile.jpg")
-//            val uploadTask = storageRef.putFile(uri)
-//
-//            uploadTask.addOnFailureListener { exception ->
-//                Toast.makeText(context, "Failed to upload profile picture!", Toast.LENGTH_LONG).show()
-//                onFailure(exception)
-//            }.addOnSuccessListener { taskSnapshot ->
-//                taskSnapshot.storage.downloadUrl.addOnSuccessListener { downloadUri ->
-//                    onSuccess(downloadUri)
-//                }.addOnFailureListener { exception ->
-//                    Toast.makeText(context, "Failed to retrieve download URL!", Toast.LENGTH_LONG).show()
-//                    onFailure(exception)
-//                }
-//            }
-//        } ?: run {
-//            Toast.makeText(context, "User ID is null", Toast.LENGTH_LONG).show()
-//        }
-//    }
-//
-//    fun getUserDetails(onUserDetailsFetched: (User?) -> Unit) {
-//        uid?.let { userId ->
-//            userRef.child(userId).addListenerForSingleValueEvent(object : ValueEventListener {
-//                override fun onDataChange(snapshot: DataSnapshot) {
-//                    val user = snapshot.getValue(User::class.java)
-//                    onUserDetailsFetched(user)
-//                }
-//
-//                override fun onCancelled(error: DatabaseError) {
-//                    onUserDetailsFetched(null)
-//                }
-//            })
-//        } ?: run {
-//            onUserDetailsFetched(null)
-//        }
-//    }
-
+    private fun addUserToDatabase() {
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(tag, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(tag, "Error adding document", e)
+            }
+    }
 
 }
